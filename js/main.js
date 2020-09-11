512379d0796d8ce653f7c8e6bce17a0a6b06339b
Bigmath <tatevikatayan@yahoo.com>
2020-09-11 10:02:05 +0300

    My work

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(window).on("load",function(){
+    $("a[rel='m_PageScroll2id']").mPageScroll2id();
+    /*Video background*/ 
+    $('#header').vide('./video/nature',{
+        bgColor: '#2e4133'
+    });
+});
